8751be7c0c044ce50542ceba88384290815e544d
Eiko <gaia.e.h@gmail.com>
2022-06-30 23:23:58 +0800

    应该修好了筛歌逻辑

--- src/mixins/songFilter.ts
+++ src/mixins/songFilter.ts
@@ -1,6 +1,7 @@
 /**
  * 输入原歌单和设置，然后一通操作后输出筛选后的歌单列表
  * 输出的格式为 [{id:"歌曲 ID",rank:"符合抽歌要求的难度"}]
+ * To do: 数据部分使用定数，就不用这么傻逼了
  * @param {Array} songlist
  * @param {Object} inputSetting
  * @returns {Array}
@@ -61,9 +62,9 @@ export default function songFilter(
   console.log("筛了版本的歌单：", tempSongList);
 
   //#region 筛歌过程
-  // 为了保证筛歌过程都是字符串，呕
-  setting.lvMin = setting.lvMin.toString();
-  setting.lvMax = setting.lvMax.toString();
+  // 屌，这里先转为数字才行
+  setting.lvMin = Number.parseInt(setting.lvMin as string);
+  setting.lvMax = Number.parseInt(setting.lvMax as string);
   // 高低一致指的是在多选难度时，最低和最高的设置完全一致
   // 这时候就跟单难度的判断方法一样了
   let 高低一致 =
@@ -74,10 +75,11 @@ export default function songFilter(
 
   // 如果是单一难度的话
   if (高低一致 === true || setting.lvMultiple === false) {
-    let 抽歌等级 = setting.lvMin;
+    let 抽歌等级: number | string = setting.lvMin;
     if (setting.lvMinPlus === true) {
       抽歌等级 = 抽歌等级 + "+";
     }
+    // 对临时歌单进行处理
     tempSongList.map((song) => {
       console.log("正在处理的歌：", song);
       for (const [songRank, songDifficulty] of Object.entries(song.等级)) {
@@ -103,11 +105,163 @@ export default function songFilter(
         }
       }
     });
+  } else if (setting.lvMultiple === true && 高低一致 === false) {
+    // 如果是 范围难度 且 不是同样的设置
+    if (setting.lvMin === setting.lvMax) {
+      console.log("等级的数字部分相同");
+      // 如果数字部分相同的话
+      // 上面已经判断过带不带加号的情况了，这里就不用判断了
+      let 抽歌等级 = setting.lvMin;
+      // 对临时歌单进行处理
+      tempSongList.map((song) => {
+        console.log("正在处理的歌：", song);
+        for (const [songRank, songDifficulty] of Object.entries(song.等级)) {
+          console.log("正在判断的难度和等级：", songRank, songDifficulty);
+          if (setting.rank.includes(songRank)) {
+            console.log("这个难度符合要求：", songRank);
+            if (抽歌等级 === Number.parseInt(songDifficulty as string)) {
+              console.log("等级也符合要求：", songDifficulty);
+
+              let songPreview = {
+                id: song.id,
+                rank: songRank,
+                preview: {
+                  name: song.曲名,
+                  rank: songRank,
+                  lv: songDifficulty,
+                },
+              };
+
+              console.log("output 被塞了这个：", songPreview);
+              output.push(songPreview);
+            }
+          }
+        }
+      });
+    } else {
+      // 如果数字部分不同的话
+      console.log("等级的数字部分不同", setting.lvMin, setting.lvMax);
+      console.log(
+        "低等级带加号：",
+        setting.lvMinPlus,
+        "高等级带加号：",
+        setting.lvMaxPlus
+      );
+
+      // 对临时歌单进行处理
+      tempSongList.map((song) => {
+        console.log("---分割线---");
+
+        console.log("正在处理的歌：", song);
+        for (const [songRank, songDifficulty] of Object.entries(song.等级)) {
+          console.log("正在判断的难度和等级：", songRank, songDifficulty);
+          if (setting.rank.includes(songRank)) {
+            console.log("这个难度符合要求：", songRank);
+
+            // 预设结果
+            let result = false;
+
+            // 麻鬼烦的等级判定
+            // 先判断整数范围
+            // 如果某首歌是 12+ 的话就在 11+ ~ 13 里面，这个肯定没问题，嗯🚩
+            // 同理，11/11+ 和 13/13+ 目前也是在范围内，下面再判断边缘情况
+            // 抽 11+ ~ 12 也是没问题的，11/12+ 的情况在下面会判断
+            if (
+              Number.parseInt(songDifficulty as string) >= setting.lvMin &&
+              Number.parseInt(songDifficulty as string) <= setting.lvMax
+            ) {
+              console.log("等级的数字部分在设置范围内：", songDifficulty);
+              result = true;
+            }
+
+            console.log(
+              "等级整数等于lvMin：",
+              Number.parseInt(songDifficulty as string) === setting.lvMin
+            );
+            console.log(
+              "结尾没有+：",
+              (songDifficulty as string)[
+                (songDifficulty as string).length - 1
+              ] !== "+"
+            );
+
+            // 再判断边缘情况
+            // 如果抽到 11 的话，就不在 11+ ~ 13 的范围了嘛
+            // 如果是抽 11 ~ 13 就不用这个判断了
+            if (
+              // what if 11/11+
+              Number.parseInt(songDifficulty as string) === setting.lvMin &&
+              // what if 要求结尾是+
+              setting.lvMinPlus === true &&
+              // what if 结尾没有+
+              (songDifficulty as string)[
+                (songDifficulty as string).length - 1
+              ] !== "+"
+            ) {
+              // 就不在抽歌范围了
+              result = false;
+              // 举例：抽 11+ ~ 12，上面筛出来一首 11
+              // 那么 11 取整得到 11，设置要求带加号，但是 11 的最后一位不是 +，所以为 false
+              // 筛出来一首 11+ 的话，取整得到 11，最后一位是 +，所以无动作，继承上面的 判定结果 = true
+            }
+
+            console.log(
+              "等级整数等于lvMax：",
+              Number.parseInt(songDifficulty as string) === setting.lvMax
+            );
+            console.log(
+              "结尾有+：",
+              (songDifficulty as string)[
+                (songDifficulty as string).length - 1
+              ] === "+"
+            );
+
+            // 判断最高等级的就是反过来的，我也不知道为什么这么写，但是感觉就该这样，脑子不太行。
+            if (
+              // what if 13/13+
+              Number.parseInt(songDifficulty as string) === setting.lvMax &&
+              // what if 要求结尾不是+
+              setting.lvMaxPlus !== true &&
+              // what if 结尾有+
+              (songDifficulty as string)[
+                (songDifficulty as string).length - 1
+              ] === "+"
+            ) {
+              // 就不在抽歌范围了
+              result = false;
+              // 举例：抽 11+ ~ 12，上面筛出来一首 12+
+              // 那么 12+ 取整得到 12，设置要求不带加号，但是 12+ 的最后一位是 +，所以为 false
+              // 筛出来一首 12 的话，取整得到 12，最后一位不是 +，所以无动作，继承上面的 判定结果 = true
+            }
+
+            if (result === true) {
+              console.log("等级符合要求：", songDifficulty);
+
+              let songPreview = {
+                id: song.id,
+                rank: songRank,
+                preview: {
+                  name: song.曲名,
+                  rank: songRank,
+                  lv: songDifficulty,
+                },
+              };
+
+              console.log("output 被塞了这个：", songPreview);
+              output.push(songPreview);
+            }
+          }
+        }
+      });
+    }
   }
 
   //#endregion
 
   console.timeEnd("筛歌计时器");
   console.log("筛歌结束");
+
+  // 挂到外面方便测试，虽然 TS 会报错但是能用
+  // window.aaa = output;
   return output;
 }
--- src/stores/songlist.ts
+++ src/stores/songlist.ts
@@ -2,42 +2,40 @@ import { defineStore } from "pinia";
 import type ISong from "@/interface/ISong";
 import { toRaw } from "vue";
 
-
 export const useSonglistStore = defineStore({
-  id: 'songlist',
+  id: "songlist",
   state: (): IState => ({
     originSonglist: [],
     currentSong: {
       id: -1,
-      "分类": '某个分类',
-      "曲名": "油婶么呢？",
-      "等级": {
-        "B": "m",
-        "A": "a",
-        "E": "i",
-        "M": "D",
-        "R": "X"
+      分类: "某个分类",
+      曲名: "油婶么呢？",
+      等级: {
+        B: "m",
+        A: "a",
+        E: "i",
+        M: "D",
+        R: "X",
       },
-      "类型": "谱面类型",
-      "版本": "版本",
-      "封面": undefined,
+      类型: "谱面类型",
+      版本: "版本",
+      封面: undefined,
     },
-    currentRank: '',
-    rollSonglist: []
+    currentRank: "",
+    rollSonglist: [],
   }),
   getters: {
-    // 嗨呀烦死了
+    // !!AnyScript
     getRawRollsonglist(state): any {
-      return toRaw(state.rollSonglist)
-    }
+      return toRaw(state.rollSonglist);
+    },
   },
-  actions: {}
+  actions: {},
 });
 
 interface IState {
-  originSonglist: Array<ISong>,
-  currentSong: ISong,
-  currentRank: string,
-  rollSonglist: Array<any>
+  originSonglist: Array<ISong>;
+  currentSong: ISong;
+  currentRank: string;
+  rollSonglist: Array<any>;
 }
-
