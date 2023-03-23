a74fe0286d701c025996b563ab25d0a22d13d641
Eiko <gaia.e.h@gmail.com>
2023-03-24 00:42:35 +0800

    又写了点

--- src/views/Detail/DetailView.vue
+++ src/views/Detail/DetailView.vue
@@ -1,5 +1,15 @@
 <template>
   <h1>Detail Song</h1>
   <p>{{ $route.params.id }}</p>
+  <pre>{{ song }}</pre>
   <a href="#" onclick="history.back()">Back</a>
 </template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+import { useSonglistStore } from '@/stores/songlist';
+
+const route = useRoute();
+const songlistStore = useSonglistStore();
+const song = songlistStore.originSonglist.find(song => song.id === Number.parseInt(route.params.id as string));
+</script>
