79140ee03676c792897434eb3728cf62ec975b69
Eiko <gaia.e.h@gmail.com>
2023-07-10 17:51:14 +0800

    版本号

--- src/views/Roll/RollIndexView.vue
+++ src/views/Roll/RollIndexView.vue
@@ -4,7 +4,7 @@
   <RollButtons @rollClicked="roll" :buttonDisable="buttonDisable"></RollButtons>
 
   <div class="footer">
-    <p>ver 0.6.1dev-20230615.01</p>
+    <p>ver 0.6.2-20230710.01</p>
   </div>
 </template>
 
