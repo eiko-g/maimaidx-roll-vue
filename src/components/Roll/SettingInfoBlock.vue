26834ad6614667ff340489608976da0b86e1155d
Eiko <gaia.e.h@gmail.com>
2023-08-12 00:20:34 +0800

    更新歌单至 1.30-D

--- src/components/Roll/SettingInfoBlock.vue
+++ src/components/Roll/SettingInfoBlock.vue
@@ -25,7 +25,7 @@
     </p>
     <p>
       <b>抽选歌单：</b>
-      <span class="songlist">ver.CH1.30-C</span>
+      <span class="songlist">ver.CH1.30-D</span>
     </p>
   </div>
 </template>
--- src/views/LoadingView.vue
+++ src/views/LoadingView.vue
@@ -31,7 +31,7 @@ onMounted(async () => {
   //   }, 800);
   // } else {
   push("歌单加载中……");
-  let response = await fetch("./data/maimaiDXCN_2023.json?ver=20230710.01"),
+  let response = await fetch("./data/maimaiDXCN_2023.json?ver=20230812.01"),
     json: IJson;
   if (response.ok) {
     json = await response.json();
